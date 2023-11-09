8ded835e19804430b034936bac92e637460b381d
Ansab369 <ansab.dev@gmail.com>
2023-11-10 02:45:15 +0530

    added the comments

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,10 @@ import 'package:school_management_system/presentation/splash/splash_screen.dart'
 import 'package:school_management_system/provider/teacher_provider/teacher_provider.dart';
 
 void main() {
+  // Initialize WidgetsFlutterBinding.
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Run the MyApp widget.
   runApp(const MyApp());
 }
 
@@ -15,18 +18,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Use MultiProvider to provide the TeacherProvider to the app.
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => TeacherProvider()),
       ],
       child: MaterialApp(
+        // Disable the debug show checked mode banner.
         debugShowCheckedModeBanner: false,
+
+        // Set the theme to use deep purple as the seed color.
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        // home: StudentDashboard(),
-        // home: AttendanceScreen(),
+
+        // Set the home page to the SplashScreen.
         home: SplashScreen(),
       ),
     );
